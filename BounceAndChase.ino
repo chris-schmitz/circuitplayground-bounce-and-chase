be7f642ea27660b0fc8d9c4ae6b0a88a54ba591a
chris-schmitz <schmitz.chris@gmail.com>
2017-07-14 09:27:17 -0500

    Adds better labeling

--- BounceAndChase.ino
+++ BounceAndChase.ino
@@ -1,35 +1,48 @@
 #include <Adafruit_CircuitPlayground.h>
 
-unsigned long rangeBottom = 0;
-unsigned long currentPointInRange = 0;
-long rangeTop = 1000;
-long triggerLED_0 = rangeTop * 0;
-long triggerLED_1 = rangeTop * .10;
-long triggerLED_2 = rangeTop * .20;
-long triggerLED_3 = rangeTop * .30;
-long triggerLED_4 = rangeTop * .40;
-long triggerLED_5 = rangeTop * .50;
-long triggerLED_6 = rangeTop * .60;
-long triggerLED_7 = rangeTop * .70;
-long triggerLED_8 = rangeTop * .80;
-long triggerLED_9 = rangeTop * .90;
 
-unsigned long rangeBottom_RIGHT = 0;
-unsigned long currentPointInRange_RIGHT = 0;
-long rangeTop_RIGHT = 1800;
-long triggerLED_0_RIGHT = rangeTop * 0;
-long triggerLED_1_RIGHT = rangeTop * .10;
-long triggerLED_2_RIGHT = rangeTop * .20;
-long triggerLED_3_RIGHT = rangeTop * .30;
-long triggerLED_4_RIGHT = rangeTop * .40;
-long triggerLED_5_RIGHT = rangeTop * .50;
-long triggerLED_6_RIGHT = rangeTop * .60;
-long triggerLED_7_RIGHT = rangeTop * .70;
-long triggerLED_8_RIGHT = rangeTop * .80;
-long triggerLED_9_RIGHT = rangeTop * .90;
+
+// Break this state machine logic out into a class and instantiate two instances
+// instead of duplicating it like this.
+unsigned long rangeBottom_A = 0;
+unsigned long currentPointInRange_A = 0;
+long rangeTop_A = 1600;
+long trigger_0_A = rangeTop_A * 0;
+long trigger_1_A = rangeTop_A * .10;
+long trigger_2_A = rangeTop_A * .20;
+long trigger_3_A = rangeTop_A * .30;
+long trigger_4_A = rangeTop_A * .40;
+long trigger_5_A = rangeTop_A * .50;
+long trigger_6_A = rangeTop_A * .60;
+long trigger_7_A = rangeTop_A * .70;
+long trigger_8_A = rangeTop_A * .80;
+long trigger_9_A = rangeTop_A * .90;
+
+unsigned long rangeBottom_B= 0;
+unsigned long currentPointInRange_B = 0;
+long rangeTop_B = 1000;
+long trigger_0_B = rangeTop_B * 0;
+long trigger_1_B = rangeTop_B * .10;
+long trigger_2_B = rangeTop_B * .20;
+long trigger_3_B = rangeTop_B * .30;
+long trigger_4_B = rangeTop_B * .40;
+long trigger_5_B = rangeTop_B * .50;
+long trigger_6_B = rangeTop_B * .60;
+long trigger_7_B = rangeTop_B * .70;
+long trigger_8_B = rangeTop_B * .80;
+long trigger_9_B = rangeTop_B * .90;
+
+
+#define ORANGE 0xFF6319
+#define BLUE 0x3857FB
+#define DARKBLUE 0x251F68
+#define OFFWHITE 0xE0DFD5
+#define GREEN 0xB3DB7F
+#define OFF 0x000000
+
 
 void setup() {
-  Serial.begin(9600);
+  // Serial.begin(9600);
   CircuitPlayground.begin();
 }
 
@@ -41,117 +54,134 @@ void loop() {
 
 void chase(long currentMillis) {
 
-  currentPointInRange = currentMillis - rangeBottom;
+  currentPointInRange_A = currentMillis - rangeBottom_A;
 
-  if (currentPointInRange >= rangeTop) {
-    // CircuitPlayground.clearPixels();
-    Serial.println("===================");
-    rangeBottom = currentMillis;
+  if (currentPointInRange_A >= rangeTop_A) {
+    rangeBottom_A = currentMillis;
     return;
   }
 
   // ------------------------------------------ //
 
-  if (currentPointInRange <= triggerLED_0) {
-    CircuitPlayground.setPixelColor(0, 0, 255, 0);
-    Serial.println("LED 0");
+  if (currentPointInRange_A <= trigger_0_A) {
+    CircuitPlayground.setPixelColor(0, DARKBLUE);
     return;
   }
 
-  if (currentPointInRange <= triggerLED_1) {
-    CircuitPlayground.setPixelColor(1, 0, 255, 0);
-    Serial.println("LED 1");
+  if (currentPointInRange_A <= trigger_1_A) {
+    CircuitPlayground.setPixelColor(1, DARKBLUE);
     return;
   }
 
-  if (currentPointInRange <= triggerLED_2) {
-    CircuitPlayground.setPixelColor(2, 0, 255, 0);
-    Serial.println("LED 2");
+  if (currentPointInRange_A <= trigger_2_A) {
+    CircuitPlayground.setPixelColor(2, DARKBLUE);
     return;
   }
 
-  if (currentPointInRange <= triggerLED_3) {
-    CircuitPlayground.setPixelColor(3, 0, 255, 0);
-    Serial.println("LED 3");
+  if (currentPointInRange_A <= trigger_3_A) {
+    CircuitPlayground.setPixelColor(3, DARKBLUE);
     return;
   }
 
-  if (currentPointInRange <= triggerLED_4) {
-    CircuitPlayground.setPixelColor(4, 0, 255, 0);
-    Serial.println("LED 4");
+  if (currentPointInRange_A <= trigger_4_A) {
+    CircuitPlayground.setPixelColor(4, DARKBLUE);
     return;
   }
 
-  if (currentPointInRange <= triggerLED_5) {
-    CircuitPlayground.setPixelColor(0, 0, 0, 0);
-    Serial.println("LED 5");
+  if (currentPointInRange_A <= trigger_5_A) {
+    CircuitPlayground.setPixelColor(0, OFF);
     return;
   }
 
-  if (currentPointInRange <= triggerLED_6) {
-    CircuitPlayground.setPixelColor(1, 0, 0, 0);
-    Serial.println("LED 6");
+  if (currentPointInRange_A <= trigger_6_A) {
+    CircuitPlayground.setPixelColor(1, OFF);
     return;
   }
 
-  if (currentPointInRange <= triggerLED_7) {
-    CircuitPlayground.setPixelColor(2, 0, 0, 0);
-    Serial.println("LED 7");
+  if (currentPointInRange_A <= trigger_7_A) {
+    CircuitPlayground.setPixelColor(2, OFF);
     return;
   }
 
-  if (currentPointInRange <= triggerLED_8) {
-    CircuitPlayground.setPixelColor(3, 0, 0, 0);
-    Serial.println("LED 8");
+  if (currentPointInRange_A <= trigger_8_A) {
+    CircuitPlayground.setPixelColor(3, OFF);
     return;
   }
 
-  if (currentPointInRange <= triggerLED_9) {
-    CircuitPlayground.setPixelColor(4, 0, 0, 0);
-
-    Serial.println("LED 9");
+  if (currentPointInRange_A <= trigger_9_A) {
+    CircuitPlayground.setPixelColor(4, OFF);
     return;
   }
 }
 
 void bounce(long currentMillis) {
 
-  currentPointInRange_RIGHT = currentMillis - rangeBottom_RIGHT;
+  currentPointInRange_B = currentMillis - rangeBottom_B;
 
+  // Trigger new range bottom
+  if (currentPointInRange_B >= rangeTop_B) {
+
+    rangeBottom_B = currentMillis;
+    return;
+  }
 
   // ------------------------------------------ //
-  if (currentPointInRange_RIGHT >= rangeTop_RIGHT) {
-    // CircuitPlayground.clearPixels();
-    // CircuitPlayground.setPixelColor(6, 0, 0, 0);
-
-    Serial.println("RIGHT LED OFF");
-    rangeBottom_RIGHT = currentMillis;
+  if (currentPointInRange_B <= trigger_0_B) {
+    CircuitPlayground.setPixelColor(5, ORANGE);
     return;
   }
 
-
-  if (currentPointInRange_RIGHT <= triggerLED_0_RIGHT) {
-    CircuitPlayground.setPixelColor(6, 0, 255, 0);
-    Serial.println("Right LED ON");
+  if (currentPointInRange_B <= trigger_1_B) {
+    CircuitPlayground.setPixelColor(5, OFF);
+    CircuitPlayground.setPixelColor(6, ORANGE);
     return;
   }
 
-  if (currentPointInRange_RIGHT <= triggerLED_7_RIGHT) {
-    CircuitPlayground.setPixelColor(6, 0, 0, 0);
-    Serial.println("LED 0");
+  if (currentPointInRange_B <= trigger_2_B) {
+    CircuitPlayground.setPixelColor(6, OFF);
+    CircuitPlayground.setPixelColor(7, ORANGE);
     return;
   }
 
-  if (currentPointInRange_RIGHT <= triggerLED_9_RIGHT) {
-    CircuitPlayground.setPixelColor(6, 0, 255, 0);
-    Serial.println("Right LED ON");
+  if (currentPointInRange_B <= trigger_3_B) {
+    CircuitPlayground.setPixelColor(7, OFF);
+    CircuitPlayground.setPixelColor(8, ORANGE);
     return;
   }
 
+  if (currentPointInRange_B <= trigger_4_B) {
+    CircuitPlayground.setPixelColor(8, OFF);
+    CircuitPlayground.setPixelColor(9, ORANGE);
+    return;
+  }
 
-  // if (currentPointInRange_RIGHT <= triggerLED_6_RIGHT) {
-  //   CircuitPlayground.setPixelColor(6, 0, 255, 0);
-  //   Serial.println("LED 0");
-  //   return;
-  // }
+  if (currentPointInRange_B <= trigger_5_B) {
+    CircuitPlayground.setPixelColor(9, OFF);
+    CircuitPlayground.setPixelColor(8, ORANGE);
+    return;
+  }
+
+  if (currentPointInRange_B <= trigger_6_B) {
+    CircuitPlayground.setPixelColor(8, OFF);
+    CircuitPlayground.setPixelColor(7, ORANGE);
+    return;
+  }
+
+  if (currentPointInRange_B <= trigger_7_B) {
+    CircuitPlayground.setPixelColor(7, OFF);
+    CircuitPlayground.setPixelColor(6, ORANGE);
+    return;
+  }
+
+  if (currentPointInRange_B <= trigger_8_B) {
+    CircuitPlayground.setPixelColor(6, OFF);
+    CircuitPlayground.setPixelColor(5, ORANGE);
+    return;
+  }
+
+  if (currentPointInRange_B <= trigger_9_B) {
+    CircuitPlayground.setPixelColor(5, BLUE);
+    return;
+  }
+
 }
